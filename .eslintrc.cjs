a56e5b5cd0f4e85abb1a6dc9d056e022eb0efbfc
Precious Okolaa <pokolaa@webmd.net>
2026-04-27 10:47:06 +0100

    ci: add ESLint config and fix CI npm registry

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2022: true },
-  extends: ['plugin:vue/vue3-recommended'],
+  env: { browser: true, es2021: true },
+  extends: ['plugin:vue/vue3-essential'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   rules: {
     'vue/multi-word-component-names': 'off',
   },
